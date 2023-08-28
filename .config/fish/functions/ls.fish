305679182f374868117d77ee7b3962ef90e33422
Christian Metts <xian@mintchaos.com>
2023-08-28 11:07:43 -0700

    adding yabai/skhd

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ls --wraps='ls --color=tty' --wraps=nnn --description 'alias ls=nnn'
-  nnn  $argv;
-end
+# function ls --wraps='ls --color=tty' --wraps=nnn --description 'alias ls=nnn'
+#   nnn  $argv;
+# end
